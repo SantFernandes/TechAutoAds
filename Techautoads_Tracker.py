f35ee6bb990bd56b3dddd78d994d908e8c31ff80
SantFernandesS30 <GeovaneFernandes@hotmail.com>
2025-03-13 11:36:59 -0300

    Removendo comando Git inválido do código

--- Techautoads_Tracker.py
+++ Techautoads_Tracker.py
@@ -1,4 +1,5 @@
 git config --global user.name "SantFernandesS30"
 git config --global user.email "GeovaneFernandes@hotmail.com"
 
+# git config --global user.name "SantFernandesS30"  # ? Isso não deveria estar aqui!
 
